8d3def701befb6a32be9cc55a9b8308a3dd0dea4
Naresh Kumar <nareshjdh31@example.com>
2026-01-04 01:10:36 +0530

    Update team section and add local images

--- Frontend/src/components/team/Team.jsx
+++ Frontend/src/components/team/Team.jsx
@@ -9,20 +9,20 @@ const Title = ({ title, desc }) => (
 );
 
 const teamMembers = [
+  { name: 'GPT-Bros', role: 'The Team' },
   { name: 'Yash Agrawal', role: 'Team Lead' },
   { name: 'Naresh Kumar', role: 'UI/UX designer' },
   { name: 'Swaraj Ku. Sahoo', role: 'Technical Lead' },
   { name: 'Shantanu Ku. Adhikari', role: 'ML Lead' },
-  { name: 'Subrat Ku. Malla', role: 'Content Lead' },
+  
 ];
 
 const memberImages = [
-  'https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmVzc2lvbmFsJTIwcGVvcGxlfGVufDB8fDB8fHww',
-  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmVzc2lvbmFsJTIwcGVvcGxlfGVufDB8fDB8fHww',
-  'https://images.unsplash.com/photo-1655249481446-25d575f1c054?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHByb2Zlc3Npb25hbCUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D',
-  'https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  '/team/theteam.jpeg',
+  '/team/yash.jpeg',
+  '/team/naresh.jpeg',
+  '/team/swaraj.jpeg',
+  '/team/shantanu.jpeg',
 ];
 
 const Team = () => {
